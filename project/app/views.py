6bd8cafe5da4c673039e73f1d0dc66f834224ae3
Eric Hanchrow <eric.hanchrow@gmail.com>
2024-07-29 13:50:59 -0700

    Make the view template-y

--- project/app/views.py
+++ project/app/views.py
@@ -1,8 +1,7 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 
 
 def duh(request):
-    return HttpResponse("<html>My Samoyed is <em>really</em> hairy.</html>")
+    return render(request, "welcome.html")
