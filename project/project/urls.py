d37803be007f23ffc9753c06be58f710f76eec01
Eric Hanchrow <eric.hanchrow@gmail.com>
2024-07-29 14:01:42 -0700

    Add the debug toolbar

--- project/project/urls.py
+++ project/project/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import include, path
+from debug_toolbar.toolbar import debug_toolbar_urls
 
 urlpatterns = [
     path("", include("app.urls")),
+    path("accounts/", include("django.contrib.auth.urls")),
     path("admin/", admin.site.urls),
-]
+] + debug_toolbar_urls()
