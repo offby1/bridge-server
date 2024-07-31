6565166784061d1aab5602941480ddd30eee5766
Eric Hanchrow <eric.hanchrow@gmail.com>
2024-07-30 18:01:24 -0700

    New failing test

--- project/app/tests.py
+++ project/app/tests.py
@@ -1,3 +1,4 @@
+import pytest
 from django.contrib import auth
 from django.test import Client
 
@@ -18,3 +19,16 @@ def test_player_names_are_links_to_detail_page(db):
     link = p.as_link()
     assert ">Bob," in link
     assert "href='/player/" in link
+
+
+def test_no_more_than_four_players_per_table(db):
+    t = Table.objects.create(name="The Table")
+
+    for username in ("Bob", "Carol", "Ted", "Alice"):
+        u = auth.models.User.objects.create(username=username)
+        Player.objects.create(user=u, table=t)
+
+    u = auth.models.User.objects.create(username="Spock")
+    with pytest.raises(Exception) as e:
+        Player.objects.create(user=u, table=t)
+    assert "more than four" in str(e.value)
