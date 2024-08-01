c2390bc4eed88970934be77f994ad89c4d152016
Eric Hanchrow <eric.hanchrow@gmail.com>
2024-08-01 05:04:13 -0700

    Passing test for the recent auth thing

--- project/app/tests.py
+++ project/app/tests.py
@@ -2,6 +2,7 @@ import pytest
 from django.contrib import auth
 from django.db import DatabaseError
 from django.test import Client
+from django.urls import reverse
 
 from .models import Player, Seat, Table
 
@@ -29,15 +30,26 @@ def test_no_bogus_directions(db):
         Seat.objects.create(table=t, direction="X")
 
 
-def test_no_more_than_four_players_per_table(db):
+@pytest.fixture()
+def usual_setup(db):
     t = Table.objects.create(name="The Table")
 
     Seat.create_for_table(t)
     for username, direction in (("Bob", "N"), ("Carol", "E"), ("Ted", "S"), ("Alice", "W")):
         s = Seat.objects.get(table=t, direction=direction)
-        u = auth.models.User.objects.create(username=username)
+        u = auth.models.User.objects.create_user(username=username, password=username)
         Player.objects.create(user=u, seat=s)
 
+
+def test_no_more_than_four_players_per_table(usual_setup):
     u = auth.models.User.objects.create(username="Spock")
     with pytest.raises(DatabaseError):
-        Player.objects.create(user=u, seat=s)
+        Player.objects.create(user=u, seat=Seat.objects.first())
+
+
+def test_only_bob_can_see_bobs_cards(usual_setup):
+    c = Client()
+    assert c.login(username="Bob", password="Bob"), "I guess I don't know how to make a test log in"
+    response = c.get(reverse("app:player", kwargs=dict(pk=1)))
+
+    assert response.context["show_cards_for"] == ["Bob"]
