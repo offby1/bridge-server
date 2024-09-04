574430e248285b69cee57ef4ceff3c6fb83425a4
Eric Hanchrow <eric.hanchrow@gmail.com>
2024-09-04 10:09:06 -0700

    Seed for a bot that calls and/or plays

---
+++ project/app/management/commands/bot.py
@@ -0,0 +1,28 @@
+import requests
+import retrying
+from django.core.management.base import BaseCommand
+from sseclient import SSEClient
+
+
+def is_requests_error(exception):
+    return isinstance(
+        exception,
+        (requests.exceptions.HTTPError, requests.exceptions.ConnectionError),
+    )
+
+
+class Command(BaseCommand):
+    @retrying.retry(wait_exponential_multiplier=1000, retry_on_exception=is_requests_error)
+    def run_forever(self):
+        while True:
+            messages = SSEClient(
+                "http://localhost:8000/events/partnerships/",
+            )
+            for msg in messages:
+                self.stdout.write(f"Ooh ooh Mr Kotter {vars(msg)=}")
+
+    def handle(self, *args, **options):
+        try:
+            self.run_forever()
+        except KeyboardInterrupt:
+            pass
