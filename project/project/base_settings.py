7a60f3cc4c079dfcc26e0b6bffb3b4ac8ee0cbea
Eric Hanchrow <eric.hanchrow@gmail.com>
2024-09-06 12:48:38 -0700

    Generate (if needed) and load secret key from a file.

--- project/project/base_settings.py
+++ project/project/base_settings.py
@@ -15,6 +15,8 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+APP_NAME = "info.offby1.bridge"
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
--- project/project/prod_settings.py
+++ project/project/prod_settings.py
@@ -1,4 +1,33 @@
-from .base_settings import *  # noqa
+import contextlib
+import os
 
-SECRET_KEY = "TODO -- read from a file that is outside the source tree, generating that file if it doesn't already exist"
+import django.core.management.utils
+import platformdirs
+
+from .base_settings import *  # noqa
+from .base_settings import APP_NAME  # redundant, but removes some complaints from emacs
+
+
+@contextlib.contextmanager
+def temp_umask(new_umask):
+    old_umask = os.umask(new_umask)
+    try:
+        yield
+    finally:
+        os.umask(old_umask)
+
+
+secret_key_path = (
+    platformdirs.site_data_path(appname=APP_NAME, ensure_exists=True) / "django_secret_key"
+)
+if not secret_key_path.is_file():
+    with temp_umask(0o77):
+        with open(secret_key_path, "w") as outf:
+            outf.write(django.core.management.utils.get_random_secret_key())
+    print(f"Created secret key at {secret_key_path}")
+
+with open(secret_key_path) as inf:
+    SECRET_KEY = inf.read()
+    print(f"read SECRET_KEY from {secret_key_path=}")
+
 DEBUG = False
