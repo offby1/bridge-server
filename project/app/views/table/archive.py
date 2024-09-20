969d5b2c1458c296a9aa9ce8eeefb3c9df2d36ca
Eric Hanchrow <eric.hanchrow@gmail.com>
2024-09-20 14:45:40 -0700

    "Minimum viable product" : review of the hand

--- project/app/views/table/archive.py
+++ project/app/views/table/archive.py
@@ -1,9 +1,16 @@
-from django.http import (
-    HttpResponse,
-)
+from django.shortcuts import get_object_or_404
+from django.template.response import TemplateResponse
+
+from app.models import Table
 
 
-def archive_view(request, *args, **kwargs):
-    return HttpResponse(
-        f"Imagine I am an archive of some completed table {request=} {args=} {kwargs=}"
+def archive_view(request, pk):
+    t = get_object_or_404(Table, pk=pk)
+    return TemplateResponse(
+        request,
+        "table_archive.html",
+        context={
+            "table": t,
+            "show_auction_history": True,
+        },
     )
