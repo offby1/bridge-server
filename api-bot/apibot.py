9a1bc65b2aad49d00bc0567813dbeb10d813dc13
Eric Hanchrow <eric.hanchrow@gmail.com>
2024-10-02 07:53:32 -0700

    Log in as bob, not ana
    Bob shows up at the club more often :-)

--- api-bot/apibot.py
+++ api-bot/apibot.py
@@ -31,7 +31,7 @@ def _run_forever() -> None:
     host = "https://erics-work-macbook-pro.tail571dc2.ts.net/"
     logger.debug("Connecting to %s", host)
 
-    my_name = "ana"
+    my_name = "bob"
     session = requests.Session()
     session.auth = (my_name, ".")
 
