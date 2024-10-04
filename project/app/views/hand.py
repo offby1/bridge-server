670e9fab550e0c896abe920a21a9885cfc49000f
Eric Hanchrow <eric.hanchrow@gmail.com>
2024-10-04 12:49:44 -0700

    Replacing replacing replacing

--- project/app/views/hand.py
+++ project/app/views/hand.py
@@ -1,5 +1,20 @@
+from django.core.paginator import Paginator
 from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render
+
+import app.models
 
 
 def hand_list_view(request: HttpRequest) -> HttpResponse:
-    return HttpResponse("whatchoo lookin' at")
+    hand_list = (
+        app.models.Hand.objects.all()
+    )  # TODO -- filter to those that should be visible by request.user
+    paginator = Paginator(hand_list, 15)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+    context = {
+        "page_obj": page_obj,
+        "total_count": app.models.Hand.objects.count(),
+    }
+
+    return render(request, "hand_list.html", context=context)
