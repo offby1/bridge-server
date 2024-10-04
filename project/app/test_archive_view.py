a2117e054e01fc822dbeced649ee84f79e076446
Eric Hanchrow <eric.hanchrow@gmail.com>
2024-10-04 01:25:01 -0700

    Dis go boom
    dunno why

--- project/app/test_archive_view.py
+++ project/app/test_archive_view.py
@@ -19,6 +19,30 @@ def test_archive_view(usual_setup: None, rf: Any) -> None:
     _four_hands_context_for_table(request, t, as_dealt=True)
 
 
+def test_for_more_smoke(usual_setup, rf) -> None:
+    t: Table | None = Table.objects.first()
+    assert t is not None
+
+    t = set_auction_to(Bid(level=1, denomination=Suit.CLUBS), t)
+
+    # all players have just one suit, so it's easy to play all the cards
+    while True:
+        assert t is not None
+        legal_cards = t.current_hand.xscript.legal_cards()
+        if not legal_cards:
+            break
+        chosen_card = legal_cards[0]
+
+        t.current_hand.add_play_from_player(player=t.current_hand.xscript.player, card=chosen_card)
+        t = Table.objects.get(pk=t.pk)
+
+    request = rf.get("/woteva/")
+    north = Player.objects.get_by_name("Jeremy Northam")
+    request.user = north.user
+    print(hand_archive_view(request, t.current_hand.pk))
+    assert "cat" == "dog"
+
+
 def test_final_score(usual_setup: None, rf: Any) -> None:
     north = Player.objects.get_by_name("Jeremy Northam")
 
