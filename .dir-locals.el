759b3bb47c693c0ade10834bfa447da159c74edb
Eric Hanchrow <eric.hanchrow@gmail.com>
2024-10-03 18:01:49 -0700

    Add "ruff" ... maybe that'll make emacs' lsp mode work better

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,1 +1,8 @@
-((python-mode . ((lsp-pylsp-server-command . ("poetry" "run" "pylsp")))))
+(
+ (python-mode
+  .
+  (
+   (lsp-pylsp-server-command . ("poetry" "run" "pylsp"))
+   (eval . (add-to-list 'exec-path (concat (file-name-as-directory (car (process-lines "poetry" "env" "info" "--path"))) "bin")))
+   ))
+ )
