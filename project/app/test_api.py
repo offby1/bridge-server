2628836f322b01bab022a66bdcc781b2375bdcb1
Eric Hanchrow <eric.hanchrow@gmail.com>
2024-10-04 12:10:12 -0700

    Summary: Don't assume the first board's primary key is 1
    But why tf isn't it??

--- project/app/test_api.py
+++ project/app/test_api.py
@@ -2,6 +2,7 @@ import json
 
 import app.models.player
 import app.views.drf_views
+from app.models import Board
 
 
 def test_card_visibility(usual_setup, rf, settings):
@@ -16,11 +17,13 @@ def test_card_visibility(usual_setup, rf, settings):
     north = app.models.player.Player.objects.get_by_name("Jeremy Northam")
     request.user = north.user
 
-    actual_board = json.loads(v(request, pk=1).render().content)
+    expected_model_board = Board.objects.first()
 
-    actual_north_cards = actual_board["north_cards"]
+    actual_serialized_board = json.loads(v(request, pk=expected_model_board.pk).render().content)
+
+    actual_north_cards = actual_serialized_board["north_cards"]
     assert actual_north_cards == "♣2♣3♣4♣5♣6♣7♣8♣9♣T♣J♣Q♣K♣A"
 
-    assert "south_cards" not in actual_board
+    assert "south_cards" not in actual_serialized_board
 
     # TODO -- make opening lead (from East), then check south cards again; this time they should be visible.
