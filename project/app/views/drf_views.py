9913d96ae5bc34ad71bed584f696292377d8435f
Eric Hanchrow <eric.hanchrow@gmail.com>
2024-10-16 08:23:17 -0700

    Factor out new "censored_data_dict" method

--- project/app/views/drf_views.py
+++ project/app/views/drf_views.py
@@ -30,8 +30,34 @@ class BoardViewSet(viewsets.ModelViewSet):
     serializer_class = BoardSerializer
     permission_classes = (permissions.IsAuthenticated,)
 
+    def _censored_data_dict(self, as_viewed_by: Player | None, board: Board) -> dict[str, str]:
+        data_dict = {
+            attr: getattr(board, attr) for attr in ("ns_vulnerable", "ew_vulnerable", "dealer")
+        }
+
+        if as_viewed_by is None:
+            data_dict["where_are_the_damned_cards"] = f"{as_viewed_by=} so no cards for you"
+        else:
+            hand = as_viewed_by.hand_at_which_board_was_played(board)
+
+            if hand is not None:
+                for seat in bridge.seat.Seat:
+                    display_and_control = _display_and_control(
+                        seat=seat,
+                        hand=hand,
+                        as_viewed_by=as_viewed_by,
+                        as_dealt=hand.is_complete,
+                    )
+                    seat_name = seat.name.lower()
+                    attribute = f"{seat_name}_cards"
+
+                    if display_and_control["display_cards"]:
+                        data_dict[attribute] = getattr(board, attribute)
+
+        return data_dict
+
     def retrieve(self, request: AuthedHttpRequest, pk=None) -> Response:
-        as_viewed_by: Player | None = request.user.player
+        as_viewed_by = request.user.player
         assert as_viewed_by is not None
 
         try:
@@ -39,27 +65,7 @@ class BoardViewSet(viewsets.ModelViewSet):
         except Board.DoesNotExist:
             return Response(status=status.HTTP_404_NOT_FOUND)
 
-        data_dict = {
-            attr: getattr(the_board, attr) for attr in ("ns_vulnerable", "ew_vulnerable", "dealer")
-        }
-
-        hand = as_viewed_by.hand_at_which_board_was_played(the_board)
-
-        if hand is not None:
-            for seat in bridge.seat.Seat:
-                display_and_control = _display_and_control(
-                    seat=seat,
-                    hand=hand,
-                    as_viewed_by=as_viewed_by,
-                    as_dealt=hand.is_complete,
-                )
-                seat_name = seat.name.lower()
-                attribute = f"{seat_name}_cards"
-
-                if display_and_control["display_cards"]:
-                    data_dict[attribute] = getattr(the_board, attribute)
-
-        return Response(data=data_dict)
+        return Response(data=self._censored_data_dict(as_viewed_by=as_viewed_by, board=the_board))
 
 
 class CallViewSet(viewsets.ModelViewSet):
