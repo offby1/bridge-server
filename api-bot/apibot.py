feac702243e3c21c381ffee0f640227ad04c9e13
Eric Hanchrow <eric.hanchrow@gmail.com>
2024-10-24 11:08:22 -0700

    Untested, but reasonable-looking, code to deal with the new message

--- api-bot/apibot.py
+++ api-bot/apibot.py
@@ -54,6 +54,21 @@ def dispatch(msg: Any, session: requests.Session) -> bool:
     return False
 
 
+def find_my_player_json_thingy(
+    *, session: requests.Session, host: str, my_name: str
+) -> dict[str, str]:
+    url = f"{host}/api/players/"
+    response = session.get(url)
+    response.raise_for_status()
+
+    all_players = response.json()
+    for p in all_players:
+        if p["name"] == my_name:
+            return p
+    msg = f"er, uh, I can't find {my_name} in the output from {url}"
+    raise Exception(msg)
+
+
 def _run_forever() -> None:
     host = "https://erics-work-macbook-pro.tail571dc2.ts.net/"
     logger.debug("Connecting to %s", host)
@@ -62,25 +77,19 @@ def _run_forever() -> None:
     session = requests.Session()
     session.auth = (my_name, ".")
 
-    response = session.get(f"{host}/api/players/")
-    response.raise_for_status()
-
-    all_players = response.json()
-    my_table_url = None
-    for p in all_players:
-        if p["name"] == my_name:
-            my_table_url = p["current_table"]
-            break
-    else:
-        msg = f"Cannot find user named {my_name=}"
-        raise Exception(msg)
+    p = find_my_player_json_thingy(
+        session=session, host=host, my_name=my_name
+    )  # n.b. https://www.youtube.com/watch?v=g2Xk_dTn2x4&t=27s
+    my_table_url = p["current_table"]
 
     while True:
-        table = session.get(my_table_url).json()
+        table = session.get(
+            my_table_url
+        ).json()  # we keep fetching this because it changes with each new hand
         logger.info(f"{my_name=} {table=}")
         current_hand_url = table["current_hand"]
         current_hand_pk = session.get(current_hand_url).json()["pk"]
-        events_url = f"{host}/events/hand/{current_hand_pk}"
+        events_url = f"{host}/events/player/system:player:{p['pk']}/"
         logger.info(f"{current_hand_url=} {current_hand_pk=}; fetching events from {events_url}")
         messages = SSEClient(events_url)
 
@@ -95,5 +104,11 @@ run_forever = retrying.retry(
 )(_run_forever)
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.DEBUG)
+    logging.basicConfig(
+        # https://docs.python.org/3/library/logging.html#logrecord-attributes
+        format="{asctime} {levelname:5} {name} {filename} {lineno} {funcName} {message}",
+        level=logging.DEBUG,
+        datefmt="%Y-%m-%dT%H:%M:%S%z",
+        style="{",
+    )
     run_forever()
