a297c3e5ebc7e0b64e61c5c3a528d5c326f7e6ba
Eric Hanchrow <eric.hanchrow@gmail.com>
2024-11-03 13:03:27 -0800

    Whop perftest_table_view until it becomes perftest_hand_view

--- project/app/management/commands/perftest_hand_view.py
+++ project/app/management/commands/perftest_hand_view.py
@@ -3,8 +3,8 @@ from __future__ import annotations
 import sys
 import timeit
 
-from app.models import Player, Table
-from app.views import table
+from app.models import Hand, Player
+from app.views import hand
 from django.core.management.base import BaseCommand
 from django.test.client import RequestFactory
 
@@ -15,11 +15,11 @@ class Command(BaseCommand):
         request = rf.get("/woteva/")
         request.user = Player.objects.get_by_name("bob").user
 
-        t = Table.objects.first()
-        assert t is not None
+        h = Hand.objects.first()
+        assert h is not None
 
         number = 5
-        total_seconds = timeit.timeit(lambda: table.table_detail_view(request, t.pk), number=number)
+        total_seconds = timeit.timeit(lambda: hand.hand_detail_view(request, h.pk), number=number)
         sys.stderr.write(
             f"{total_seconds=} for {number=} calls, mean {total_seconds / number} seconds\n",
         )
