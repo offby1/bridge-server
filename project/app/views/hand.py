offby1/bridge-server

commit 438c0feff3ba5732cace3228d19124f989f7ef81

Mollify django-fastdev by using its "ifexists" filter

project/app/templates/auction.html
@@ -1,33 +1,34 @@
-{% if show_auction_history %}
-    <div id="auction" style="border: 1px dotted; overflow: auto; height: 10em">
-        <table class="table">
-            <th>
+{% load fastdev %}
+{% ifexists show_auction_history %}
+<div id="auction" style="border: 1px dotted; overflow: auto; height: 10em">
+    <table class="table">
+        <th>
+            <tr>
+                <td>
+                    <i>West</i>
+                </td>
+                <td>
+                    <i>North</i>
+                </td>
+                <td>
+                    <i>East</i>
+                </td>
+                <td>
+                    <i>South</i>
+                </td>
+            </tr>
+        </th>
+        <tbody>
+            {% for row in hand.auction.fancy_HTML_display %}
                 <tr>
-                    <td>
-                        <i>West</i>
-                    </td>
-                    <td>
-                        <i>North</i>
-                    </td>
-                    <td>
-                        <i>East</i>
-                    </td>
-                    <td>
-                        <i>South</i>
-                    </td>
+                    {% for call in row %}
+                        <td>
+                            {% if call is not None %}{{ call|safe }}{% endif %}
+                        </td>
+                    {% endfor %}
                 </tr>
-            </th>
-            <tbody>
-                {% for row in hand.auction.fancy_HTML_display %}
-                    <tr>
-                        {% for call in row %}
-                            <td>
-                                {% if call is not None %}{{ call|safe }}{% endif %}
-                            </td>
-                        {% endfor %}
-                    </tr>
-                {% endfor %}
-            </tbody>
-        </table>
-    </div>
-{% endif %}
+            {% endfor %}
+        </tbody>
+    </table>
+</div>
+{% endifexists %}

project/app/templates/base.html
@@ -1,3 +1,4 @@
+{% load fastdev %}
 {% load static %}
 {% load partials %}
 {% load base_extras %}
@@ -25,70 +26,72 @@
             | <a href="{% url "app:table-list" %}">Tables</a>
             | <a href="{% url "app:players" %}">Players</a>
             | <a href="{{ VERSION|gitlab_link }}">version <tt>{{ VERSION }}</tt></a>
-            {% if debug %}<h2>| It's debugging time!!</h2>{% endif %}
+            {% ifexists debug %}
+            <h2>| It's debugging time!!</h2>
+        {% endifexists %}
+    </div>
+    <div class="row">
+        <div class="col">
+            <div id="hello-user">
+                {% if user.is_authenticated %}
+                    Hello,
+                    {% if user.player %}
+                        {{ user.player.as_link }}.
+                    {% else %}
+                        {{ user }}.
+                    {% endif %}
+                </div>
+                <div>
+                    {# TODO -- reload the whole page, or somehow update the "Hello" message, which includes whether we're a bot or not #}
+                    {% if user.player %}
+                        <div class="form-check form-switch">
+                            <input class="form-check-input"
+                                   type="checkbox"
+                                   role="switch"
+                                   id="bot-plays-for-me-switch"
+                                   hx-post="{% url 'app:bot-checkbox-toggle' user.player.pk %}"
+                                   hx-target="#hello-user"
+                                   {% if user.player.allow_bot_to_play_for_me %}checked{% endif %} />
+                            <label class="form-check-label" for="bot-plays-for-me-switch">Let the bot play for me</label>
+                        </div>
+                    {% endif %}
+                </div>
+            </div>
+            {% block upper-right-column %}
+            {% endblock upper-right-column %}
         </div>
         <div class="row">
             <div class="col">
-                <div id="hello-user">
-                    {% if user.is_authenticated %}
-                        Hello,
-                        {% if user.player %}
-                            {{ user.player.as_link }}.
-                        {% else %}
-                            {{ user }}.
-                        {% endif %}
-                    </div>
-                    <div>
-                        {# TODO -- reload the whole page, or somehow update the "Hello" message, which includes whether we're a bot or not #}
-                        {% if user.player %}
-                            <div class="form-check form-switch">
-                                <input class="form-check-input"
-                                       type="checkbox"
-                                       role="switch"
-                                       id="bot-plays-for-me-switch"
-                                       hx-post="{% url 'app:bot-checkbox-toggle' user.player.pk %}"
-                                       hx-target="#hello-user"
-                                       {% if user.player.allow_bot_to_play_for_me %}checked{% endif %} />
-                                <label class="form-check-label" for="bot-plays-for-me-switch">Let the bot play for me</label>
-                            </div>
-                        {% endif %}
-                    </div>
-                </div>
-                {% block upper-right-column %}
-                {% endblock upper-right-column %}
-            </div>
-            <div class="row">
-                <div class="col">
-                    <form method="post" action="{% url 'logout' %}">
-                        {% csrf_token %}
-                        <input type="submit" value="logout" />
-                        <input type="hidden" name="next" value="/" />
-                    </form>
-                {% else %}
-                    <a href="{% url 'app:signup' %}">Sign Up</a>
-                    |<a href="{% url 'login' %}">Login</a>
-                {% endif %}
-            </div>
+                <form method="post" action="{% url 'logout' %}">
+                    {% csrf_token %}
+                    <input type="submit" value="logout" />
+                    <input type="hidden" name="next" value="/" />
+                </form>
+            {% else %}
+                <a href="{% url 'app:signup' %}">Sign Up</a>
+                |<a href="{% url 'login' %}">Login</a>
+            {% endif %}
         </div>
-        {% if messages %}
-            <div>
-                <ul class="messages">
-                    {% for message in messages %}
-                        <li {% if message.tags %}class="{{ message.tags }}"{% endif %}>{{ message }}</li>
-                    {% endfor %}
-                </ul>
-            </div>
-        {% endif %}
-        <div id="content" class="container">
-            {% block content %}
-            {% endblock content %}
+    </div>
+    {% if messages %}
+        <div>
+            <ul class="messages">
+                {% for message in messages %}
+                    <li {% if message.tags %}class="{{ message.tags }}"{% endif %}>{{ message }}</li>
+                {% endfor %}
+            </ul>
         </div>
-        <script src="{% static 'app/bootstrap-5.3.3-dist/js/bootstrap.bundle.min.js' %}"></script>
-        <script src="{% static 'django_eventstream/eventsource.min.js' %}"></script>
-        <script src="{% static 'django_eventstream/reconnecting-eventsource.js' %}"></script>
-        <script src="{% static 'app/htmx.min.js' %}"></script>
-        <script src="{% static 'app/sse.js' %}"></script>
-        {% block scripts %}
-        {% endblock scripts %}
-    </body>
+    {% endif %}
+    <div id="content" class="container">
+        {% block content %}
+        {% endblock content %}
+    </div>
+    <script src="{% static 'app/bootstrap-5.3.3-dist/js/bootstrap.bundle.min.js' %}"></script>
+    <script src="{% static 'django_eventstream/eventsource.min.js' %}"></script>
+    <script src="{% static 'django_eventstream/reconnecting-eventsource.js' %}"></script>
+    <script src="{% static 'app/htmx.min.js' %}"></script>
+    <script src="{% static 'app/sse.js' %}"></script>
+    {% block scripts %}
+    {% endblock scripts %}
+</body>
 </html>

project/project/dev_settings.py
@@ -1,12 +1,10 @@
 import logging
-import os
 
 from .base_settings import *  # noqa
 from .base_settings import TEMPLATES
 
-# Alas, django_fastdev breaks a t est and I'm too lazy to figure out why
-if os.environ.get("PYTEST_VERSION") is None:
-    INSTALLED_APPS.append("django_fastdev")
+INSTALLED_APPS.append("django_fastdev")
+FASTDEV_STRICT_IF = True
 
 # https://docs.djangoproject.com/en/5.0/topics/templates/#django.template.backends.django.DjangoTemplates says
 #   'debug': ... defaults to the value of the DEBUG setting.

commit 18701f3034a055fbf86d3e242d44b6cf9e96ffbe

Fix long-standing bug: don't display calls once auction is settled
Introduced in

    438c0feff3ba5732cace3228d19124f989f7ef81
    Commit:     Eric Hanchrow <eric.hanchrow@gmail.com>
    CommitDate: 2024-11-03 15:25:36 -0800

    Mollify django-fastdev by using its "ifexists" filter

project/app/templates/auction.html
@@ -1,34 +1,34 @@
 {% load fastdev %}
-{% ifexists show_auction_history %}
-<div id="auction" style="border: 1px dotted; overflow: auto; height: 10em">
-    <table class="table">
-        <th>
-            <tr>
-                <td>
-                    <i>West</i>
-                </td>
-                <td>
-                    <i>North</i>
-                </td>
-                <td>
-                    <i>East</i>
-                </td>
-                <td>
-                    <i>South</i>
-                </td>
-            </tr>
-        </th>
-        <tbody>
-            {% for row in hand.auction.fancy_HTML_display %}
+{% if show_auction_history %}
+    <div id="auction" style="border: 1px dotted; overflow: auto; height: 10em">
+        <table class="table">
+            <th>
                 <tr>
-                    {% for call in row %}
-                        <td>
-                            {% if call is not None %}{{ call|safe }}{% endif %}
-                        </td>
-                    {% endfor %}
+                    <td>
+                        <i>West</i>
+                    </td>
+                    <td>
+                        <i>North</i>
+                    </td>
+                    <td>
+                        <i>East</i>
+                    </td>
+                    <td>
+                        <i>South</i>
+                    </td>
                 </tr>
-            {% endfor %}
-        </tbody>
-    </table>
-</div>
-{% endifexists %}
+            </th>
+            <tbody>
+                {% for row in hand.auction.fancy_HTML_display %}
+                    <tr>
+                        {% for call in row %}
+                            <td>
+                                {% if call is not None %}{{ call|safe }}{% endif %}
+                            </td>
+                        {% endfor %}
+                    </tr>
+                {% endfor %}
+            </tbody>
+        </table>
+    </div>
+{% endif %}

project/app/views/hand.py
@@ -63,6 +63,7 @@ def _bidding_box_context_for_hand(request, hand):
         "bidding_box_buttons": buttons,
         "bidding_box_partial_endpoint": reverse("app:bidding-box-partial", args=[hand.pk]),
         "display_bidding_box": display_bidding_box,
+        "show_auction_history": hand.auction.status is bridge.auction.Auction.Incomplete,
     }
 
 