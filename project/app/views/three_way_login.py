ea06b15a6e84c75295edc689316cec26d955db70
Eric Hanchrow <eric.hanchrow@gmail.com>
2024-12-18 15:34:53 -0800

    Escape the HTML!

--- project/app/views/three_way_login.py
+++ project/app/views/three_way_login.py
@@ -6,6 +6,7 @@ from django.conf import settings
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User
 from django.http import HttpRequest, HttpResponse, HttpResponseForbidden
+from django.utils.html import escape
 
 logger = logging.getLogger(__name__)
 
@@ -16,12 +17,12 @@ def three_way_login_view(request: HttpRequest) -> HttpResponse:
     if user and user.is_authenticated:
         msg = f"{user=} is authenticated -- welcome"
         logger.info(msg)
-        return HttpResponse(msg)
+        return HttpResponse(escape(msg))
 
     if (auth_header := request.headers.get("Authorization")) is None:
         msg = "No Authorization header attached to my request!  Begone!"
         logger.info(msg)
-        return HttpResponseForbidden(msg)
+        return HttpResponseForbidden(escape(msg))
 
     logger.info("Maybe the auth header has a clue")
 
@@ -30,39 +31,39 @@ def three_way_login_view(request: HttpRequest) -> HttpResponse:
     except ValueError as e:
         msg = f"Something about {e}"
         logger.info(msg)
-        return HttpResponseForbidden(msg)
+        return HttpResponseForbidden(escape(msg))
 
     if auth_flavor != "Basic":
         msg = f"I don't do {auth_flavor} authorization, sorry dude"
         logger.info(msg)
-        return HttpResponseForbidden(msg)
+        return HttpResponseForbidden(escape(msg))
 
     try:
         decoded = base64.b64decode(b64_stuff.encode()).decode()
     except binascii.Error as e:
         msg = f"{e}, sorry"
         logger.info(msg)
-        return HttpResponseForbidden(msg)
+        return HttpResponseForbidden(escape(msg))
 
     try:
         username_or_pk, password = decoded.split(":", maxsplit=1)
     except ValueError as e:
         msg = f"{e}, sorry"
         logger.info(msg)
-        return HttpResponseForbidden(msg)
+        return HttpResponseForbidden(escape(msg))
 
     if username_or_pk.isdigit():  # TODO -- this won't work if we shift to, say, UUIDs as keys
         if (user := User.objects.filter(pk=username_or_pk).first()) is not None:
             if password == settings.API_SKELETON_KEY:
                 login(request, user)
-                return HttpResponse(f"Oh look, {user=} used the skeleton key")
+                return HttpResponse(escape(f"Oh look, {user=} used the skeleton key"))
 
     elif (user := authenticate(username=username_or_pk, password=password)) is None:
         msg = f"{username_or_pk=} {password=} just doesn't cut it, sorry"
         logger.info(msg)
-        return HttpResponseForbidden(msg)
+        return HttpResponseForbidden(escape(msg))
 
     msg = f"{user=} used a regular password. Splendid."
     logger.info(msg)
     login(request, user)
-    return HttpResponse(msg)
+    return HttpResponse(escape(msg))
