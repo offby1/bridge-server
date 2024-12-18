044fd005ea58275f803e7e33207d313a1a17296b
Eric Hanchrow <eric.hanchrow@gmail.com>
2024-12-18 15:53:19 -0800

    Try disabling sentry ...
    ... because py-spy is showing me this
    
    241.63s behind in sampling, results may be inaccurate. Try reducing the sampling rate.
    242.04s behind in sampling, results may be inaccurate. Try reducing the sampling rate.
    Collecting samples from '/bridge/.venv/bin/python /bridge/.venv/bin/daphne --verbosity 3 --bind 0.0.0.0 --port 9000 project.asgi:application --log-fmt=%(asctime)sZ %(levelname)s %(name)s %(filename)s %(funcName)s %(message)s' (python v3.13.1)
    Total Samples 21483
    GIL: 100.00%, Active: 119.00%, Threads: 212
    
      %Own   %Total  OwnTime  TotalTime  Function (filename)
    100.00% 100.00%   87.65s    87.69s   write (sentry_sdk/profiler/transaction_profiler.py)
      9.00%   9.00%   129.7s    182.8s   _worker (concurrent/futures/thread.py)

--- project/project/prod_settings.py
+++ project/project/prod_settings.py
@@ -29,21 +29,25 @@ print(f"prod_settings: {HOST_HOSTNAME=}")
 # "production": running on my EC2 box or some other cloud server, with docker
 DEPLOYMENT_ENVIRONMENT = "production" if HOST_HOSTNAME == "ip-10-0-0-174" else "staging"
 
-sentry_sdk.init(  # type: ignore
-    dsn="https://a18e83409c4ba3304ff35d0097313e7a@o4507936352501760.ingest.us.sentry.io/4507936354205696",
-    # Set traces_sample_rate to 1.0 to capture 100%
-    # of transactions for tracing.
-    environment=DEPLOYMENT_ENVIRONMENT,
-    traces_sample_rate=1.0,
-    # Set profiles_sample_rate to 1.0 to profile 100%
-    # of sampled transactions.
-    # We recommend adjusting this value in production.
-    profiles_sample_rate=1.0,
-    integrations=[
-        DjangoIntegration(
-            middleware_spans=False,
-            signals_spans=False,
-        ),
-    ],
-    release=VERSION,
-)
+if False:
+    import sentry_sdk
+    from sentry_sdk.integrations.django import DjangoIntegration
+
+    sentry_sdk.init(  # type: ignore
+        dsn="https://a18e83409c4ba3304ff35d0097313e7a@o4507936352501760.ingest.us.sentry.io/4507936354205696",
+        # Set traces_sample_rate to 1.0 to capture 100%
+        # of transactions for tracing.
+        environment=DEPLOYMENT_ENVIRONMENT,
+        traces_sample_rate=1.0,
+        # Set profiles_sample_rate to 1.0 to profile 100%
+        # of sampled transactions.
+        # We recommend adjusting this value in production.
+        profiles_sample_rate=1.0,
+        integrations=[
+            DjangoIntegration(
+                middleware_spans=False,
+                signals_spans=False,
+            ),
+        ],
+        release=VERSION,
+    )
