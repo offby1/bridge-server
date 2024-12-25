4e4013985cffcd55d1675801fa4c8ddcf3f1b259
Eric Hanchrow <eric.hanchrow@gmail.com>
2024-12-25 14:55:30 -0600

    Make the new test check plays as well as calls

--- project/app/test_api.py
+++ project/app/test_api.py
@@ -186,14 +186,26 @@ def test_xscript_works_despite_caching_being_hard_yo(usual_setup):
     h1 = Hand.objects.first()
     assert h1 is not None
 
-    x1 = h1.get_xscript()
-    assert len(x1.auction.player_calls) == 0
+    assert len(h1.get_xscript().auction.player_calls) == 0
 
     c = Call.objects.create(serialized="1♣", hand=h1)
     c.save()
 
-    x2 = h1.get_xscript()
-    assert len(x2.auction.player_calls) == 1
+    assert len(h1.get_xscript().auction.player_calls) == 1
+
+    Call.objects.create(serialized="Pass", hand=h1)
+    Call.objects.create(serialized="Pass", hand=h1)
+    Call.objects.create(serialized="Pass", hand=h1)
+
+    assert len(h1.get_xscript().auction.player_calls) == 4
+
+    assert list(h1.get_xscript().plays()) == []
+
+    Play.objects.create(serialized="♦2", hand=h1)
+
+    plays = list(h1.get_xscript().plays())
+    assert len(plays) == 1
+    assert plays[0].card.serialize() == "♦2"
 
 
 def skip_test_serialized_play(played_almost_to_completion) -> None:
