2c60acc31c279bc384de098187e182c743329d01
Eric Hanchrow <eric.hanchrow@gmail.com>
2025-01-09 12:26:16 -0800

    Include current_event_id in the table_json_view
    Mostly to be analagous to how we serialze hands.

--- project/app/views/table/misc.py
+++ project/app/views/table/misc.py
@@ -2,12 +2,16 @@ import json
 
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import get_object_or_404
+from django_eventstream import get_current_event_id  # type: ignore[import-untyped]
 
 from app.models import Table
 
 
 def table_json_view(request: HttpRequest, pk: int) -> HttpResponse:
     table = get_object_or_404(Table, pk=pk)
-    # all our caller cares about is the current_hand pk
-    payload = {"current_hand_pk": table.current_hand.pk}
+
+    payload = {
+        "current_hand_pk": table.current_hand.pk,
+        "current_event_id": get_current_event_id([table.event_channel_name]),
+    }
     return HttpResponse(json.dumps(payload), headers={"Content-Type": "text/json"})
