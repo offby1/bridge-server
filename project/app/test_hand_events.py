b0944f74968322e74d644d725e97812df5d9b0bf
Eric Hanchrow <eric.hanchrow@gmail.com>
2025-01-09 12:54:46 -0800

    Rewrite some tests to not use monkeypatching

--- project/app/test_hand_events.py
+++ project/app/test_hand_events.py
@@ -1,42 +1,58 @@
 from __future__ import annotations
 
-import collections
-from typing import Any
+import json
 
 import bridge.card
 import bridge.contract
 import pytest
 from django_eventstream.models import Event  # type: ignore[import-untyped]
 
-from .models import Player, Table, hand
+from .models import Hand, Player, Table
 from .testutils import set_auction_to
 
 
-def test_auction_settled_messages(usual_setup, monkeypatch) -> None:
+class CapturedEventsFromChannel:
+    def __init__(self, channel_name: str) -> None:
+        self._channel_name = channel_name
+        self.events = []
+        self._message_ids_before = set(
+            Event.objects.filter(channel=channel_name).values_list("id", flat=True),
+        )
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.events = list(
+            Event.objects.filter(channel=self._channel_name).exclude(
+                id__in=self._message_ids_before
+            )
+        )
+        return False
+
+
+def test_auction_settled_messages(usual_setup) -> None:
     t = Table.objects.first()
     assert t is not None
     h = t.current_hand
 
-    sent_events: list[dict[str, Any]] = []
+    with CapturedEventsFromChannel(h.pk) as cap:
+        set_auction_to(
+            bridge.contract.Bid(level=1, denomination=bridge.card.Suit.DIAMONDS),
+            h,
+        )
 
-    def send_timestamped_event(*, channel: str, data: dict[str, Any]) -> None:
-        sent_events.append({"channel": channel, "data": data})
-
-    monkeypatch.setattr(hand, "send_timestamped_event", send_timestamped_event)
-
-    set_auction_to(bridge.contract.Bid(level=1, denomination=bridge.card.Suit.DIAMONDS), h)
+    assert sum(["new-call" in e.data for e in cap.events]) == 4
+    assert sum(["contract" in e.data for e in cap.events]) == 1
 
     serial_numbers = []
-    hand_event_counts_by_top_level_keys: collections.Counter[str] = collections.Counter()
-    for e in sent_events:
-        # let's only look at events for the hand
-        if e["channel"] == "1":
-            serial_numbers.append(e["data"]["serial_number"])
-            for k in e["data"]:
-                hand_event_counts_by_top_level_keys[k] += 1
+    for e in cap.events:
+        if e.data:
+            # I have no idea why I need to decode this *twice*
+            data = json.loads(json.loads(e.data))
 
-    assert hand_event_counts_by_top_level_keys["new-call"] == 4
-    assert hand_event_counts_by_top_level_keys["contract"] == 1
+            if (sn := data.get("serial_number")) is not None:
+                serial_numbers.append(sn)
 
     assert serial_numbers[0:3] == [0, 1, 2]
 
@@ -50,40 +66,28 @@ def test_player_can_always_see_played_hands(played_to_completion) -> None:
 
 
 @pytest.mark.usefixtures("played_almost_to_completion")
-def test_sends_final_score(monkeypatch) -> None:
-    sent_events = []
+def test_sends_final_score() -> None:
+    h = Hand.objects.get(pk=1)
 
-    def send_timestamped_event(channel: str, data: dict[str, Any]) -> None:
-        sent_events.append(data)
-
-    from .models import Hand, hand
-
-    monkeypatch.setattr(hand, "send_timestamped_event", send_timestamped_event)
-    h1 = Hand.objects.get(pk=1)
-    assert h1.player_who_may_play is not None
-    libPlayer = h1.player_who_may_play.libraryThing()
+    assert h.player_who_may_play is not None
+    libPlayer = h.player_who_may_play.libraryThing()
     libCard = bridge.card.Card.deserialize("♠A")
 
-    h1.add_play_from_player(player=libPlayer, card=libCard)
+    with CapturedEventsFromChannel(h.pk) as cap:
+        h.add_play_from_player(player=libPlayer, card=libCard)
 
     def sought(datum):
         return "final_score" in datum and "table" in datum
 
-    assert any(sought(d) for d in sent_events)
+    assert any(sought(d.data) for d in cap.events)
 
 
 @pytest.mark.usefixtures("played_to_completion")
 def test_sends_new_hand_event_to_table_channel() -> None:
     t1 = Table.objects.first()
     assert t1 is not None
-    filter_dict = {"channel": t1.event_channel_name}
 
-    message_ids_before = set(
-        Event.objects.filter(**filter_dict).values_list("id", flat=True),
-    )
+    with CapturedEventsFromChannel(t1.event_channel_name) as cap:
+        t1.next_board()
 
-    t1.next_board()
-
-    messages_after = Event.objects.filter(**filter_dict).exclude(id__in=message_ids_before)
-
-    assert any("new-hand" in m.data for m in messages_after)
+    assert any("new-hand" in m.data for m in cap.events)
