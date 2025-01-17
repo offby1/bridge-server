23de5f78cdad48a994b99faf34d2da6de037e403
Eric Hanchrow <eric.hanchrow@gmail.com>
2025-01-17 15:48:00 -0800

    More-accurate way to tell if we're running in prod

--- project/project/prod_settings.py
+++ project/project/prod_settings.py
@@ -16,24 +16,23 @@ print(f"prod_settings: {HOST_HOSTNAME=}")
 # "development": running on my laptop without docker
 # "staging": running on my laptop with docker
 # "production": running on my EC2 box or some other cloud server, with docker
-DEPLOYMENT_ENVIRONMENT = "production" if HOST_HOSTNAME == "ip-10-0-0-174" else "staging"
+DEPLOYMENT_ENVIRONMENT = "production" if os.getenv("COMPOSE_PROFILES") == "prod" else "staging"
 
-if DEPLOYMENT_ENVIRONMENT == "production":
-    sentry_sdk.init(  # type: ignore
-        dsn="https://a18e83409c4ba3304ff35d0097313e7a@o4507936352501760.ingest.us.sentry.io/4507936354205696",
-        # Set traces_sample_rate to 1.0 to capture 100%
-        # of transactions for tracing.
-        environment=DEPLOYMENT_ENVIRONMENT,
-        traces_sample_rate=1.0,
-        # Set profiles_sample_rate to 1.0 to profile 100%
-        # of sampled transactions.
-        # We recommend adjusting this value in production.
-        profiles_sample_rate=0.01,
-        integrations=[
-            DjangoIntegration(
-                middleware_spans=False,
-                signals_spans=False,
-            ),
-        ],
-        release=VERSION,
-    )
+sentry_sdk.init(  # type: ignore
+    dsn="https://a18e83409c4ba3304ff35d0097313e7a@o4507936352501760.ingest.us.sentry.io/4507936354205696",
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for tracing.
+    environment=DEPLOYMENT_ENVIRONMENT,
+    traces_sample_rate=1.0,
+    # Set profiles_sample_rate to 1.0 to profile 100%
+    # of sampled transactions.
+    # We recommend adjusting this value in production.
+    profiles_sample_rate=0.01,
+    integrations=[
+        DjangoIntegration(
+            middleware_spans=False,
+            signals_spans=False,
+        ),
+    ],
+    release=VERSION,
+)
