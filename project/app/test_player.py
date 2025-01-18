f6d01f4c0be8d104efb73518b7d8e34c0d978c2b
Eric Hanchrow <eric.hanchrow@gmail.com>
2025-01-18 10:47:10 -0800

    Flesh out new test

--- project/app/test_player.py
+++ project/app/test_player.py
@@ -1,9 +1,11 @@
 # Tests for the "tainting" mechanism.
+import pytest
 
-from app.models import Player
+from app.models import Board, Player, Seat, Table
 
 
-def test_tainted_player_may_not_play_that_board(nobody_seated: None) -> None:
+@pytest.fixture
+def seat_em_dano(nobody_seated: None) -> Table:
     north = Player.objects.get_by_name("Jeremy Northam")
     south = Player.objects.get_by_name("J.D. Souther")
     east = Player.objects.get_by_name("Clint Eastwood")
@@ -13,4 +15,19 @@ def test_tainted_player_may_not_play_that_board(nobody_seated: None) -> None:
     north.partner_with(south)
     east.partner_with(west)
 
-    assert north.name == "south"
+    # Not calling create_with_two_partnerships since that invokes "next_board", which I'd prefer to invoke explicitly
+    t = Table.objects.create()
+    Seat.objects.create(direction="N", player=north, table=t)
+    Seat.objects.create(direction="E", player=east, table=t)
+    Seat.objects.create(direction="S", player=south, table=t)
+    Seat.objects.create(direction="W", player=west, table=t)
+
+    return t
+
+
+def test_untainted_players_may_play_any_board(seat_em_dano) -> None:
+    # Just making sure this test isn't out of sync with the fixture
+    assert set(Board.objects.values_list("pk", flat=True)) == {1, 2}
+
+    t = seat_em_dano
+    t.next_board()
