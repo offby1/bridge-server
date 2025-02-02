716c7d30689bae8d4d1ce5b32596e1afdbc6c9ce
Eric Hanchrow <eric.hanchrow@gmail.com>
2025-02-01 17:02:22 -0800

    Putting together an alternate way to reproduce the problem ...
        AssertionError: self.currently_seated=True and yet I cannot find a table at which I am sitting

--- project/app/management/commands/repro_trouble.py
+++ project/app/management/commands/repro_trouble.py
@@ -3,7 +3,10 @@ from app.models import Hand, Player, Table
 from django.core.management.base import BaseCommand
 
 """
-We assume you've already loaded the db backup
+We assume you've already loaded the db backup; invoke me like this
+
+$ just restore < 2025-02-01T21:36:20+0000.sql && just manage repro_trouble
+
 """
 
 
@@ -11,3 +14,21 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         Hand.objects.get(pk=318).delete()
         Table.objects.get(pk=21).delete()
+
+        alexis = Player.objects.get_by_name("alexis")
+        katie = Player.objects.get_by_name("katie")
+
+        assert alexis.partner == katie
+        alexis.break_partnership()
+        alexis.partner_with(katie)
+
+        christine = Player.objects.get_by_name("christine")
+        craig = Player.objects.get_by_name("craig")
+
+        assert christine.partner == craig
+        christine.break_partnership()
+        christine.partner_with(craig)
+
+        t = Table.objects.create_with_two_partnerships(christine, katie)
+
+        print(t)
