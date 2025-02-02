ad474cf52c3785aaa0da7e6496502b3639022143
Eric Hanchrow <eric.hanchrow@gmail.com>
2025-02-01 17:42:50 -0800

    Woohoo, this reproduces the problem.
    How do I know?  When I run it, it prints out "bogons" three times:
    
    * The first time is before this script makes any changes; it shows the one that is the actual problem that I ran into.
    * The second time is after the script has deleted the bogus hand, and recreated partnerships; it's empty.
    * The third time is after I create a new table.  This proves that whatever code I've got that's supposed to prevent this
    from happening, ain't workin'.

--- project/app/management/commands/repro_trouble.py
+++ project/app/management/commands/repro_trouble.py
@@ -10,25 +10,57 @@ $ just restore < 2025-02-01T21:36:20+0000.sql && just manage repro_trouble
 """
 
 
+def scan_for_bogons():
+    bogon_tuples = set()
+
+    for p in Player.objects.all():
+        for b in p.boards_played.all():
+            qs = Hand.objects.filter(
+                board=b,
+                table__in=Table.objects.filter(
+                    pk__in=p.historical_seat_set.values_list("table_id", flat=True).all()
+                ).all(),
+            ).all()
+
+            if qs.count() > 1:
+                for h in qs.order_by("id"):
+                    bogon_tuples.add(
+                        (p.name, f"Board #{b.display_number} ({b.tournament.pk})", h.pk)
+                    )
+    import pprint
+
+    pprint.pprint(bogon_tuples)
+
+
 class Command(BaseCommand):
     def handle(self, *args, **options):
+        print("Initially:")
+        scan_for_bogons()
+
         Hand.objects.get(pk=318).delete()
         Table.objects.get(pk=21).delete()
 
         alexis = Player.objects.get_by_name("alexis")
         katie = Player.objects.get_by_name("katie")
+        christine = Player.objects.get_by_name("christine")
+        craig = Player.objects.get_by_name("craig")
+
+        for p in (alexis, katie, christine, craig):
+            p.unseat_me()
+            p.save()
 
         assert alexis.partner == katie
         alexis.break_partnership()
         alexis.partner_with(katie)
 
-        christine = Player.objects.get_by_name("christine")
-        craig = Player.objects.get_by_name("craig")
-
         assert christine.partner == craig
         christine.break_partnership()
         christine.partner_with(craig)
 
-        t = Table.objects.create_with_two_partnerships(christine, katie)
+        print("After splitsville and re-partering:")
+        scan_for_bogons()
 
-        print(t)
+        Table.objects.create_with_two_partnerships(christine, katie)
+
+        print("After creating new table:")
+        scan_for_bogons()
