2e8877b4ac742c79aa892a6f6aaf5fa0af5efa79
Eric Hanchrow <eric.hanchrow@gmail.com>
2025-02-03 09:36:46 -0800

    Create a new "unit test" ...
    ... that isn't exactly a unit test, but rather, a way to generate a fixture that puts things in the interesting state

--- project/app/test_tournaments.py
+++ project/app/test_tournaments.py
@@ -2,9 +2,11 @@ import collections
 
 import pytest
 from bridge.card import Card
+from django.contrib import auth
 
 import app.views.hand
-from app.models import Board, Hand, Player, Tournament
+from app.models import Board, Hand, Player, Table, Tournament
+import app.models.board
 
 # mumble import settings, monkeypatch, change BOARDS_PER_TOURNAMENT to 2 for convenience
 
@@ -62,3 +64,36 @@ def test_hand_from_completed_tournament_can_serialize(just_completed, rf) -> Non
     request.user = Player.objects.get_by_name("Adam West").user
     response = app.views.hand.hand_serialized_view(request, pk=1)
     print(f"{response=}")
+
+
+def test_ponder_wassup(played_almost_to_completion, everybodys_password, monkeypatch) -> None:
+    with monkeypatch.context() as m:
+        t1 = Table.objects.first()
+        assert t1 is not None
+        assert Table.objects.count() == 1
+
+        m.setattr(app.models.board, "BOARDS_PER_TOURNAMENT", 1)
+        # Create a second table in this tournament.
+        for name in ("n2", "e2", "s2", "w2"):
+            u = auth.models.User.objects.create(username=name, password=everybodys_password)
+            Player.objects.create(user=u)
+        n2 = Player.objects.get_by_name("n2")
+        n2.partner_with(Player.objects.get_by_name("s2"))
+
+        e2 = Player.objects.get_by_name("e2")
+        e2.partner_with(Player.objects.get_by_name("w2"))
+
+        Table.objects.create_with_two_partnerships(n2, e2)
+        # Complete the first table.
+
+        h1 = Hand.objects.get(pk=1)
+        west = Player.objects.get_by_name("Adam West")
+        h1.add_play_from_player(player=west.libraryThing(), card=Card.deserialize("♠A"))
+
+        # Have someone at the first table click "Next Board Plz".
+        t1.next_board()
+
+        # Now what??
+        from django.core.management import call_command
+
+        call_command("dumpdata", "--output", "now_what")
