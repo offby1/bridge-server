0f58b89be480febfd62616ddbc7d426aab4c9e84
Eric Hanchrow <eric.hanchrow@gmail.com>
2025-02-08 06:43:23 -0800

    Humanize the tournament timestamps

--- project/app/templatetags/base_extras.py
+++ project/app/templatetags/base_extras.py
@@ -1,13 +1,16 @@
 import contextlib
+from typing import Any
 
 from django import template
 from django.conf import settings
 from django.utils.html import format_html
+from django.utils.timezone import now
+import humanize
 
 register = template.Library()
 
 
-def gitlab_link(value):
+def gitlab_link(value: Any) -> str:
     version_blob = value
     del value
 
@@ -21,3 +24,12 @@ def gitlab_link(value):
 
 
 register.filter("gitlab_link", gitlab_link)
+
+
+def humanized_timestamp(value: Any) -> str:
+    delta = value - now()
+    suffix = "ago" if delta.total_seconds() < 0 else "from now"
+    return f"{value.isoformat()} ({humanize.naturaldelta(delta)} {suffix})"
+
+
+register.filter("humanized_timestamp", humanized_timestamp)
