3ee099bd49530a1c6b30b933148945a5c24aa752
Eric Hanchrow <eric.hanchrow@gmail.com>
2025-02-18 09:55:48 -0800

    Limit poking to incomplete tournaments

--- project/app/management/commands/end_all_signups.py
+++ project/app/management/commands/end_all_signups.py
@@ -6,6 +6,6 @@ from django.utils import timezone
 class Command(BaseCommand):
     def handle(self, *_args, **_options) -> None:
         now = timezone.now()
-        app.models.Tournament.objects.filter(signup_deadline__isnull=False).update(
-            signup_deadline=now
-        )
+        app.models.Tournament.objects.filter(
+            is_complete=False, signup_deadline__isnull=False
+        ).update(signup_deadline=now)
