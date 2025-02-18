0e4a92dbcf980184eb03a41b522b4322d5ac719c
Eric Hanchrow <eric.hanchrow@gmail.com>
2025-02-17 17:19:41 -0800

    Handy for manual testing

---
+++ project/app/management/commands/end_all_signups.py
@@ -0,0 +1,11 @@
+import app.models
+from django.core.management.base import BaseCommand
+from django.utils import timezone
+
+
+class Command(BaseCommand):
+    def handle(self, *_args, **_options) -> None:
+        now = timezone.now()
+        app.models.Tournament.objects.filter(signup_deadline__isnull=False).update(
+            signup_deadline=now
+        )
