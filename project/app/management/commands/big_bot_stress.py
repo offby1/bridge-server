1f45d53a6d5c35f04241c98142630066b393a6b2
Eric Hanchrow <eric.hanchrow@gmail.com>
2025-02-27 09:19:17 -0800

    More fiddling around with order of operations
    plus invoke "bring_up_all_api_bots" rather than duplicating its stuff inline

--- project/app/management/commands/big_bot_stress.py
+++ project/app/management/commands/big_bot_stress.py
@@ -6,6 +6,7 @@ from app.models.table import Table
 from app.models.signups import TournamentSignup
 from app.models.tournament import Tournament, check_for_expirations
 from django.conf import settings
+from django.core.management import call_command
 from django.core.management.base import BaseCommand, CommandError
 from django.db import transaction
 
@@ -28,47 +29,40 @@ class Command(BaseCommand):
             )
 
         t: Tournament
-        t, _ = Tournament.objects.get_or_create_tournament_open_for_signups()
-
         with transaction.atomic():
+            t, _ = Tournament.objects.get_or_create_tournament_open_for_signups()
+
             for p in (
                 Player.objects.filter(currently_seated=False)
                 .filter(partner__currently_seated=False)
                 .all()
             ):
                 if TournamentSignup.objects.filter(player__in={p, p.partner}).exists():
+                    # TODO -- maybe yoink 'em outta that tournament and into this one?
                     self.stderr.write(f"{p.name} is already signed up for some tournament")
                 else:
                     t.sign_up(p)
 
             t.signup_deadline = datetime.datetime.now(tz=datetime.UTC)
+
+            # saving, and checking for expirations, has all kindsa side effects.
+
+            # if the tournament is complete (it shouldn't be, but ...) then saving will delete all the signups, and put
+            # all the players into the lobby.
+
             t.save()
 
-        check_for_expirations(sender="big_bot_stress")
+            # - complete the tournament if all the hands have been played
+            # - add some boards to the tournament
+            # - seat everyone at newly-created tables, creating (and signing up) some synths if necessary
+            check_for_expirations(sender="big_bot_stress")
 
-        bots_enabled = []
-        player: Player
-        for player in Player.objects.filter(currently_seated=True):
-            try:
-                player.toggle_bot(True)
-            except TooManyBots:
-                self.stderr.write("Huh, I guess you *can* have too many bots")
-                break
-            except OSError as e:
-                self.stderr.write(
-                    f"{e}; I assume we're not running under docker, so ... outta here",
-                )
-                break
-            else:
-                bots_enabled.append(f"{player.name} ({player.pk})")
-                self.stderr.write(f"{player.name} done")
+            call_command("bring_up_all_api_bots")
 
-        tables_updated: list[str] = []
-        for table in Table.objects.all():
-            table.tempo_seconds = 0
-            table.save()
-            tables_updated.append(str(table.pk))
+            tables_updated: list[str] = []
+            for table in Table.objects.all():
+                table.tempo_seconds = 0
+                table.save()
+                tables_updated.append(str(table.pk))
 
-        self.stderr.write(f"Sped up tables {', '.join(tables_updated)}")
-
-        self.stderr.write(f"Enabled bots for players {', '.join(bots_enabled)} ")
+            self.stderr.write(f"Sped up tables {', '.join(tables_updated)}")
