0dc3f6acc925ccbf7b8619bca1f29df268690ed0
Eric Hanchrow <eric.hanchrow@gmail.com>
2025-03-08 07:57:09 -0800

    Move generation of toy pairs to a new function
    Still failing :-(

--- project/app/test_movements.py
+++ project/app/test_movements.py
@@ -1,4 +1,6 @@
 import collections
+import operator
+from typing import TYPE_CHECKING, Any
 
 import more_itertools
 import pytest
@@ -7,46 +9,57 @@ from django.contrib import auth
 
 from app.models import Player, Tournament
 import app.models.board
-from app.utils.movements import BoardGroup, Movement, Pair
+from app.utils.movements import BoardGroup, Movement, Pair, PhantomPair
 
 from .testutils import play_out_hand
 
 
+def example_pairs(n: int) -> list[Pair]:
+    def ns_first(pair_name: str) -> tuple[bool, int]:
+        direction, number = pair_name.split()
+        return (bool(direction == "EW"), int(number))
+
+    north_south_names = [
+        "NS 1",
+        "NS 2",
+        "NS 3",
+        "NS 4",
+        "NS 5",
+        "NS 6",
+        "NS 7",
+    ]
+    east_west_names = [
+        "EW 1",
+        "EW 2",
+        "EW 3",
+        "EW 4",
+        "EW 5",
+        "EW 6",
+        "EW 7",
+    ]
+    for index, name in enumerate(
+        # Produce a list where all the NS names come first, followed by all the EW names.  That's the order that the movement will put them anyway.
+        sorted(
+            list(more_itertools.interleave(north_south_names, east_west_names))[0:n], key=ns_first
+        )
+    ):
+        yield Pair(names=name, id=frozenset([2 * index, 2 * index + 1]))
+
+
 @pytest.mark.django_db
 def test_movement_class() -> None:
-    pairs = [
-        Pair(names=s, id=frozenset([2 * index, 2 * index + 1]))
-        for index, s in enumerate(
-            [
-                "NS 1",
-                "NS 2",
-                "NS 3",
-                "NS 4",
-                "NS 5",
-                "NS 6",
-                "NS 7",
-                "EW 1",
-                "EW 2",
-                "EW 3",
-                "EW 4",
-                "EW 5",
-                "EW 6",
-            ]
-        )
-    ]
-
-    for num_pairs in range(3, len(pairs) + 1):
-        num_tables, _ = Movement.num_tables(num_pairs=num_pairs)
+    for num_pairs in range(3, 13):
         for boards_per_round in (2, 3, 4, 5):
             t = Tournament.objects.create()
-            print(f"\n\n{num_pairs=} {boards_per_round=}\n")
-
-            all_pairs: set[Pair] = set()
-
             da_movement = Movement.from_pairs(
-                boards_per_round_per_table=boards_per_round, pairs=pairs[0:num_pairs], tournament=t
+                boards_per_round_per_table=boards_per_round,
+                pairs=list(example_pairs(num_pairs)),
+                tournament=t,
             )
 
+            print(f"\n\n{num_pairs=} {boards_per_round=}\n")
+
+            # Ensure there's never more than one phantom
             for table_number, rounds in da_movement.items():
                 for r in rounds:
                     quartet, board_group = r.quartet, r.board_group
@@ -57,32 +70,24 @@ def test_movement_class() -> None:
                     else:
                         assert len(normals) == 2
 
+            # Ensure every pair plays every board exactly once.
+            times_played_by_pair_board_combo = collections.defaultdict(int)
             for table_number, rounds in da_movement.items():
                 for r in rounds:
                     quartet, board_group = r.quartet, r.board_group
-                    pairs_in_this_round = set()
-                    pairs_in_this_round.add(quartet.ns)
-                    pairs_in_this_round.add(quartet.ew)
+                    if not any(isinstance(p, PhantomPair) for p in (quartet.ns, quartet.ew)):
+                        times_played_by_pair_board_combo[(quartet.ns.id, board_group)] += 1
+                        times_played_by_pair_board_combo[(quartet.ew.id, board_group)] += 1
 
-            if all_pairs:
-                import pprint
+            print(f"{times_played_by_pair_board_combo=}")
 
-                # Ensure every pair plays in every round
-                assert (
-                    all_pairs == pairs_in_this_round
-                ), f"{pprint.pformat(all_pairs)} but {pprint.pformat(pairs_in_this_round)}"
+            # If there's an odd number of pairs, we get a phantom pair
+            if num_pairs % 2 == 0:
+                expected = {boards_per_round}
             else:
-                all_pairs = pairs_in_this_round
+                expected = {boards_per_round, boards_per_round - 1}
 
-            pair_board_combos: collections.Counter[tuple[Pair, BoardGroup]] = collections.Counter()
-            for table_number, rounds in da_movement.items():
-                for r in rounds:
-                    quartet, board_group = r.quartet, r.board_group
-                    pair_board_combos[(quartet.ns, board_group)] += 1
-                    pair_board_combos[(quartet.ew, board_group)] += 1
-
-            [(pair, count)] = pair_board_combos.most_common(1)
-            assert count == 1
+            assert set(times_played_by_pair_board_combo.values()) == expected
 
 
 def test_pairs_and_boards_move(db, everybodys_password) -> None:
