3182aba064c69d87b2d65c45b31accb912727624
Eric Hanchrow <eric.hanchrow@gmail.com>
2025-05-10 07:22:35 -0700

    Make "x" in the URL a "folder"

--- project/app/urls.py
+++ project/app/urls.py
@@ -89,5 +89,5 @@ urlpatterns = [
         tournament_void_signup_deadline_view,
         name="tournament-void-signup-deadline",
     ),
-    path("x/<pk>/", hand_xperimental_view, name="hand-xperimental-view"),
+    path("x/hand/<pk>/", hand_xperimental_view, name="hand-xperimental-view"),
 ]
