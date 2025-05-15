b586b50d582caa98f6093d0780c686db4ff72c6a
Eric Hanchrow <eric.hanchrow@gmail.com>
2025-05-15 14:36:19 -0700

    Tweak INTERNAL_IPS so that we see debug stuff when running via orbstack.
    It's entirely possible that this will affect prod, too :-|

--- project/project/base_settings.py
+++ project/project/base_settings.py
@@ -127,6 +127,7 @@ STORAGES = {"staticfiles": {"BACKEND": "whitenoise.storage.CompressedManifestSta
 
 INTERNAL_IPS = [
     "127.0.0.1",
+    "192.168.97.1",  # when running via orbstack.  No idea if I can count on this or what.
 ]
 
 ROOT_URLCONF = "project.urls"
