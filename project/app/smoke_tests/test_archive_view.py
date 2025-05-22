0302144e8b7267f4042bc42a6ed16eec259a072d
Eric Hanchrow <eric.hanchrow@gmail.com>
2025-05-22 11:43:42 -0700

    Move our one smoke test into a new subdirectory.

---
+++ project/app/smoke_tests/test_archive_view.py
@@ -0,0 +1,16 @@
+from typing import Any
+
+
+from django.contrib.auth.models import AnonymousUser
+
+from app.models import Hand
+
+from app.views.hand import _four_hands_context_for_hand
+
+
+def test_archive_view(usual_setup: Hand, rf: Any) -> None:
+    h = usual_setup
+    request = rf.get("/woteva/", data={"pk": h.pk})
+    request.user = AnonymousUser()
+    # We're just testing for the absence of an exception
+    _four_hands_context_for_hand(request=request, hand=h, as_dealt=True)
