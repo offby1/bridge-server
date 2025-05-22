43525b51bba29307d917396239ec6950439e8e24
Eric Hanchrow <eric.hanchrow@gmail.com>
2025-05-22 15:21:06 -0700

    Greatly fancify generation of hands

--- project/app/smoke_tests/test_important_views.py
+++ project/app/smoke_tests/test_important_views.py
@@ -3,11 +3,12 @@ import enum
 import itertools
 from typing import Any
 
+import pytest
 
 from django.contrib.auth.models import AnonymousUser, User
 from django.core.management import call_command
 
-from app.models import Hand, Player
+from app.models import Hand, Player, Tournament
 
 from app.views.hand import hand_archive_view, hand_detail_view
 
@@ -48,15 +49,33 @@ class TournamentState(enum.Enum):
     complete = enum.auto()
 
 
-def various_flavors_of_hand():
+@pytest.fixture
+def various_flavors_of_hand(db: Any):
+    call_command("loaddata", "smoke-case-1")
+
     hands_by_hand_state_by_tournament_state = collections.defaultdict(dict)
     for hand_state, tournament_state in itertools.product(HandState, TournamentState):
-        hands_by_hand_state_by_tournament_state[hand_state][tournament_state] = (
-            f"Imagine I'm a {hand_state} hand in a {tournament_state} tournament"
-        )
+        the_tournament = None
+        match tournament_state:
+            case TournamentState.incomplete:
+                the_tournament = Tournament.objects.get(pk=1)
+            case _:
+                assert not "I am dumb"
+
+        the_hand = None
+        match hand_state:
+            case HandState.abandoned, HandState.complete:
+                assert not "I am still dumb"
+            case HandState.incomplete:
+                the_hand = Hand.objects.get(pk=10)
+
+        the_hand.board.tournament = the_tournament
+        the_hand.board.save()
+
+        hands_by_hand_state_by_tournament_state[hand_state][tournament_state] = the_hand
 
     return dict(hands_by_hand_state_by_tournament_state)
 
 
-def test_wat():
-    assert various_flavors_of_hand() == "cat"
+def test_wat(various_flavors_of_hand):
+    assert various_flavors_of_hand == "cat"
