08ea7f6a03700538aa1fa5d628cba8ed3bf12345
Eric Hanchrow <eric.hanchrow@gmail.com>
2025-05-22 14:44:55 -0700

    Add another view, fix another error.

--- project/app/smoke_tests/test_important_views.py
+++ project/app/smoke_tests/test_important_views.py
@@ -6,10 +6,10 @@ from django.core.management import call_command
 
 from app.models import Hand, Player
 
-from app.views.hand import hand_archive_view
+from app.views.hand import hand_archive_view, hand_detail_view
 
 
-def test_archive_view(db: Any, rf: Any) -> None:
+def test_both_important_views(db: Any, rf: Any) -> None:
     call_command("loaddata", "smoke-case-1")
     hand_pk = 10
     request = rf.get("/woteva/", data={"pk": hand_pk})
@@ -25,3 +25,4 @@ def test_archive_view(db: Any, rf: Any) -> None:
     for user in (None, anonymoose, has_no_player, played_the_hand, did_not_play_the_hand):
         request.user = user
         hand_archive_view(request=request, pk=hand_pk)
+        hand_detail_view(request=request, pk=hand_pk)
--- project/app/views/hand.py
+++ project/app/views/hand.py
@@ -578,10 +578,15 @@ def _terse_description(hand: Hand) -> str:
 def hand_detail_view(request: AuthedHttpRequest, pk: PK) -> HttpResponse:
     hand: app.models.Hand = get_object_or_404(app.models.Hand, pk=pk)
 
+    login_page = HttpResponseRedirect(settings.LOGIN_URL + f"?next={request.path}")
+
+    if request.user is None:
+        return login_page
+
     # TODO -- don't require that the entire tournament be complete; instead, require only that this particular board
     # will not be played again.
     if request.user.is_anonymous and not hand.board.tournament.is_complete:
-        return HttpResponseRedirect(settings.LOGIN_URL + f"?next={request.path}")
+        return login_page
 
     player = getattr(request.user, "player", None)
 
