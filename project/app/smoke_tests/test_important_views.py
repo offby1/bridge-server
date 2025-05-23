2cb605cc9d6d3a31b93cb56e264b111b66ff491b
Eric Hanchrow <eric.hanchrow@gmail.com>
2025-05-23 07:14:37 -0700

    Wire it all up.
    It all passes?!  Seems too good to be true.

--- project/app/smoke_tests/test_important_views.py
+++ project/app/smoke_tests/test_important_views.py
@@ -21,31 +21,6 @@ from app.testutils import play_out_hand
 logger = logging.getLogger()
 
 
-def test_both_important_views(db: Any, rf: Any) -> None:
-    call_command("loaddata", "smoke-case-1")
-
-    # What is interesting about this hand?
-    # - it's incomplete (has just a single call, and no plays)
-    # - following from the above, it's not abandoned
-    # - its tournament is still running
-    hand_pk = 10
-
-    request = rf.get("/woteva/", data={"pk": hand_pk})
-
-    hand = Hand.objects.get(pk=hand_pk)
-
-    # Various flavors of user
-    anonymoose = AnonymousUser()
-    has_no_player = User.objects.get(username="admin")
-    played_the_hand = hand.players().first().user
-    did_not_play_the_hand = Player.objects.create_synthetic().user
-
-    for user in (None, anonymoose, has_no_player, played_the_hand, did_not_play_the_hand):
-        request.user = user
-        hand_archive_view(request=request, pk=hand_pk)
-        hand_detail_view(request=request, pk=hand_pk)
-
-
 class HandState(enum.Enum):
     incomplete = enum.auto()
     abandoned = enum.auto()
@@ -78,7 +53,8 @@ def _completed_tournament(db: Any):
 
     with freeze_time(Signup + datetime.timedelta(seconds=1)):
         _do_signup_expired_stuff(t)
-        h1 = t.hands().first()
+        h1: Hand | None = t.hands().first()
+        assert h1 is not None
         play_out_hand(h1)
 
     with freeze_time(PlayCompletion + datetime.timedelta(seconds=1)):
@@ -97,7 +73,9 @@ def smoke_case_1(db: Any):
 def various_flavors_of_hand(db: Any, smoke_case_1, _completed_tournament: Tournament):
     assert datetime.datetime.utcnow().year == 2000
 
-    hands_by_hand_state_by_tournament_state = collections.defaultdict(dict)
+    hands_by_hand_state_by_tournament_state: dict[HandState, dict[TournamentState, Hand]] = (
+        collections.defaultdict(dict)
+    )
     for hand_state, tournament_state in itertools.product(HandState, TournamentState):
         the_tournament = None
         match tournament_state:
@@ -135,5 +113,18 @@ def various_flavors_of_hand(db: Any, smoke_case_1, _completed_tournament: Tourna
     return dict(hands_by_hand_state_by_tournament_state)
 
 
-def test_wat(various_flavors_of_hand):
-    assert various_flavors_of_hand == "cat"
+def test_both_important_views(rf: Any, various_flavors_of_hand) -> None:
+    for hand_state, hands_by_tournament_state in various_flavors_of_hand.items():
+        for tournament_state, hand in hands_by_tournament_state.items():
+            request = rf.get("/woteva/", data={"pk": hand.pk})
+
+            # Various flavors of user
+            anonymoose = AnonymousUser()
+            has_no_player = User.objects.get(username="admin")
+            played_the_hand = hand.players().first().user
+            did_not_play_the_hand = Player.objects.create_synthetic().user
+
+            for user in (None, anonymoose, has_no_player, played_the_hand, did_not_play_the_hand):
+                request.user = user
+                hand_archive_view(request=request, pk=hand.pk)
+                hand_detail_view(request=request, pk=hand.pk)
