f02beaa7bb1d26d47badc350d7b928287f18ba2f
Eric Hanchrow <eric.hanchrow@gmail.com>
2025-05-27 08:46:01 -0700

    Ask daphne to honor "X-Forwarded-For" when logging the remote IP

--- start-daphne.sh
+++ start-daphne.sh
@@ -14,5 +14,6 @@ exec poetry run  daphne                                                         
     --verbosity  3                                                                              \
     --bind 0.0.0.0                                                                              \
     --port 9000                                                                                 \
+    --proxy-headers                                                                             \
     project.asgi:application                                                                    \
     --log-fmt="%(asctime)sZ %(levelname)s %(name)s %(filename)s %(funcName)s %(message)s"
