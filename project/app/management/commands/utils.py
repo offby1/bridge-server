066c695b77bb3a74ce27af899ab9c1efad7db4ce
Eric Hanchrow <eric.hanchrow@gmail.com>
2025-06-06 14:33:02 -0700

    Comment

--- project/app/management/commands/utils.py
+++ project/app/management/commands/utils.py
@@ -3,6 +3,7 @@ import os
 from django.conf import settings
 
 
+# Roughly, "is it safe to use hard-coded passwords, or trash the database, or create a fleet of killer bots"
 def is_safe(stderr) -> bool:
     DJANGO_SETTINGS_MODULE = os.environ.get("DJANGO_SETTINGS_MODULE")
     DOCKER_CONTEXT = os.environ.get("DOCKER_CONTEXT")
