fb8a28d4436ffd97a15d28e15295860979821f1b
Eric Hanchrow <eric.hanchrow@gmail.com>
2025-06-08 19:14:10 -0700

    Include username in logs.

--- project/app/middleware/simple_access_log.py
+++ project/app/middleware/simple_access_log.py
@@ -15,7 +15,9 @@ class RequestLoggingMiddleware:
 
         duration_ms = int(round((after - before) * 1000))
 
-        log_message_prefix = f"{request.META['REMOTE_ADDR']} {request.method}:{request.path_info} => {response.status_code}"
+        user = getattr(getattr(request, "user", None), "username", None)
+
+        log_message_prefix = f"{request.META['REMOTE_ADDR']} {user=} {request.method}:{request.path_info} => {response.status_code}"
 
         # The debug toolbar adds this header.
         if (server_timing := response.headers.get("Server-Timing")) is not None:
