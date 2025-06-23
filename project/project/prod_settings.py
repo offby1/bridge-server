d76c55b23524a8b5212fd2d8fbd51987a0fba3ec
Eric Hanchrow <eric.hanchrow@gmail.com>
2025-06-22 19:02:12 -0700

    Remove sentry

--- project/project/prod_settings.py
+++ project/project/prod_settings.py
@@ -1,9 +1,8 @@
 import os
 
-import sentry_sdk
 
 from .base_settings import *  # noqa
-from .base_settings import LOGGING, VERSION
+from .base_settings import LOGGING
 
 DEBUG = False
 
@@ -14,22 +13,3 @@ DEPLOYMENT_ENVIRONMENT = "production" if os.getenv("COMPOSE_PROFILES") == "prod"
 
 if DEPLOYMENT_ENVIRONMENT == "production":
     LOGGING["handlers"]["console"]["level"] = "INFO"
-
-# https://docs.sentry.io/platforms/python/integrations/django/
-sentry_sdk.init(  # type: ignore
-    dsn="https://a18e83409c4ba3304ff35d0097313e7a@o4507936352501760.ingest.us.sentry.io/4507936354205696",
-    # Add data like request headers and IP for users;
-    # see https://docs.sentry.io/platforms/python/data-management/data-collected/ for more info
-    send_default_pii=True,
-    environment=DEPLOYMENT_ENVIRONMENT,
-    # Set traces_sample_rate to 1.0 to capture 100%
-    # of transactions for tracing.
-    traces_sample_rate=1.0,
-    # To collect profiles for all profile sessions,
-    # set `profile_session_sample_rate` to 1.0.
-    profile_session_sample_rate=1.0,
-    # Profiles will be automatically collected while
-    # there is an active span.
-    profile_lifecycle="trace",
-    release=VERSION,
-)
