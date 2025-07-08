688a4d9a897e19fb418de2951588311eb88cbe01
Eric Hanchrow <eric.hanchrow@gmail.com>
2025-07-08 12:12:34 -0700

    Failed attempt to repro the problem.
    I ran
    
        just restore < bot-thinks-its-bobs-turn-2025-07-08T18:39:37+0000.sql && just manage repro
    
    expecting to see a difference between the two sets of "prints", but they are the same, and both look correct: they agree
    with the web page, and show that "_marlo-polk" is the next caller.
    
    And yet when I actually run the bot (instead of "just manage repro") it sits there telling me that "bob is human", and
    doing nothing else.

---
+++ project/app/management/commands/repro.py
@@ -0,0 +1,18 @@
+import pprint
+
+from django.core.cache import cache
+from django.core.management.base import BaseCommand
+
+from app.management.commands.cheating_bot import get_next_hand
+
+
+class Command(BaseCommand):
+    def handle(self, *_args, **_options) -> None:
+        for _ in range(2):
+            hand_to_play = get_next_hand()
+
+            pprint.pprint(hand_to_play.get_xscript().serializable()["auction"])
+            print(f"{getattr(hand_to_play.player_who_may_call, 'name', None)=}")
+            print(f"{getattr(hand_to_play.player_who_may_play, 'name', None)=}")
+
+            cache.clear()
