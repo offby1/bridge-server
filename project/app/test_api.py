5d3d60e2f10dd12609fbf5e6ab1438f14416303d
Eric Hanchrow <eric.hanchrow@gmail.com>
2025-07-09 12:58:46 -0700

    Split the big test up into a buncha little ones.

--- project/app/test_api.py
+++ project/app/test_api.py
@@ -1,6 +1,9 @@
+from django.core.management import call_command
 import django.db.models
 from django.test import Client
 from django.urls import reverse
+
+import pytest
 
 import bridge.card
 import bridge.contract
@@ -35,7 +38,7 @@ def test_xscript_works_despite_caching_being_hard_yo(usual_setup) -> None:
     assert plays[0].card.serialize() == "♦2"
 
 
-def _no_voids():
+def _no_voids(t: Tournament):
     deck = bridge.card.Card.deck()
 
     north_cards = []
@@ -52,16 +55,18 @@ def _no_voids():
     board = Board.objects.create(
         **{
             "dealer": "N",
-            "display_number": 1
-            + Board.objects.all().aggregate(django.db.models.Max("display_number"))[
-                "display_number__max"
-            ],
+            "display_number": (
+                1
+                + Board.objects.all().aggregate(django.db.models.Max("display_number", default=0))[
+                    "display_number__max"
+                ]
+            ),
             "east_cards": "".join(east_cards),
             "ew_vulnerable": False,
             "north_cards": "".join(north_cards),
             "ns_vulnerable": True,
             "south_cards": "".join(south_cards),
-            "tournament": Tournament.objects.first(),
+            "tournament": t,
             "west_cards": "".join(west_cards),
         }
     )
@@ -78,7 +83,7 @@ def _no_voids():
     )
 
 
-def test_play_post_view(usual_setup, rf):
+def test_ppv_anon(usual_setup):
     c = Client()
 
     # Anonymous user
@@ -87,38 +92,60 @@ def test_play_post_view(usual_setup, rf):
     assert response.status_code == 302
     assert "/accounts/login/?next=/play/" in response.url
 
+
+@pytest.fixture
+def one_club_hand(db):
+    call_command("loaddata", "usual_setup")
+
     hand = Hand.objects.first()
     set_auction_to(bridge.contract.Bid(level=1, denomination=bridge.card.Suit.CLUBS), hand)
 
-    # Not seated
+    return hand
+
+
+def test_ppv_not_seated(usual_setup):
+    c = Client()
+
     player, _ = Player.objects.get_or_create_synthetic()
     c.force_login(player.user)
     response = c.post(reverse("app:play-post"), data={"card": "C2"})
     assert response.status_code == 403
     assert "not currently seated" in response.text
 
-    # Seated, but not my turn
-    player = Player.objects.exclude(pk=hand.player_who_may_play.pk).first()
-    assert player != hand.player_who_may_play
-    c.force_login(player.user)
 
-    assert hand.player_who_may_play.name == "Clint Eastwood"
-    east_cards_string = getattr(hand.board, "east_cards")
+def test_ppv_seated_but_not_my_turn(one_club_hand):
+    c = Client()
+
+    not_their_turn_player = Player.objects.exclude(pk=one_club_hand.player_who_may_play.pk).first()
+    assert not_their_turn_player != one_club_hand.player_who_may_play
+    c.force_login(not_their_turn_player.user)
+
+    assert one_club_hand.player_who_may_play.name == "Clint Eastwood"
+    east_cards_string = getattr(one_club_hand.board, "east_cards")
 
     response = c.post(reverse("app:play-post"), data={"card": east_cards_string[0:2]})
     assert response.status_code == 403
     assert "turn to play" in response.text
 
-    # My turn, but not a valid card string
-    c.force_login(hand.player_who_may_play.user)
+
+def test_ppv_invalid_card(one_club_hand):
+    c = Client()
+
+    c.force_login(one_club_hand.player_who_may_play.user)
     response = c.post(
         reverse("app:play-post"), data={"card": "not really a serialized card at all"}
     )
     assert response.status_code == 403
     assert "Cannot deserialize" in response.text
 
+
+def test_ppv_my_turn_but_not_my_card(one_club_hand):
+    c = Client()
+
+    c.force_login(one_club_hand.player_who_may_play.user)
+
     # My turn, but not my card
-    north_cards_string = getattr(hand.board, "north_cards")
+    north_cards_string = getattr(one_club_hand.board, "north_cards")
     response = c.post(
         reverse("app:play-post"),
         data={"card": north_cards_string[0:2]},
@@ -127,18 +154,17 @@ def test_play_post_view(usual_setup, rf):
     assert response.status_code == 403
     assert "don't hold" in response.text
 
+
+def test_ppv_not_following_suit(db):
+    c = Client()
+    call_command("loaddata", "usual_setup")
+    hand = _no_voids(Tournament.objects.first())
+    set_auction_to(bridge.contract.Bid(level=1, denomination=bridge.card.Suit.CLUBS), hand)
+
     # One that works
     assert hand.player_who_may_play.name == "Clint Eastwood"
-    c.force_login(hand.player_who_may_play.user)
-    response = c.post(reverse("app:play-post"), data={"card": east_cards_string[0:2]})
-    assert response.status_code == 200
 
-    # right player, and their card; but illegal because it's not following suit
-    hand: Hand = _no_voids()
-    set_auction_to(bridge.contract.Bid(level=1, denomination=bridge.card.Suit.CLUBS), hand)
-    assert hand.player_who_may_play.name == "Clint Eastwood"
     c.force_login(hand.player_who_may_play.user)
-
     response = c.post(reverse("app:play-post"), data={"card": "C4"})
     assert response.status_code == 200
 
