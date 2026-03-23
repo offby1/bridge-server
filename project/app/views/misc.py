a5c30731accfa7bdcc68c72d24a562bd3b01e758
Eric Hanchrow <eric.hanchrow@gmail.com>
2026-03-23 09:57:29 -0700

    Nix unused function

--- project/app/views/misc.py
+++ project/app/views/misc.py
@@ -1,15 +1,11 @@
 from __future__ import annotations
 
-import base64
-import binascii
 import datetime
 import functools
 import logging
-from typing import TYPE_CHECKING, Iterator
-
+from typing import Iterator
 
 from django.contrib import messages as django_web_messages
-from django.contrib.auth import authenticate
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.http import HttpRequest, HttpResponseForbidden, HttpResponseRedirect
@@ -22,43 +18,12 @@ import app.models.common
 import app.models.utils
 from app.views import Forbid
 
-if TYPE_CHECKING:
-    from django.contrib.auth.models import AbstractBaseUser
-
 logger = logging.getLogger(__name__)
 
 
 # See https://github.com/sbdchd/django-types?tab=readme-ov-file#httprequests-user-property
 class AuthedHttpRequest(HttpRequest):
     user: app.models.utils.UserMitPlaya  # type: ignore [assignment]
-
-
-# https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Authorization#basic_authentication_2
-def authenticate_from_basic_auth(request: AuthedHttpRequest) -> AbstractBaseUser | None:
-    header = request.headers.get("Authorization")
-    if header is None:
-        logger.debug("No 'Authorization' header")
-        return None
-
-    basic, data = header.split(" ", maxsplit=1)
-    if basic.lower() != "basic":
-        logger.debug("First word isn't 'basic'")
-        return None
-    try:
-        decoded = base64.b64decode(data).decode()
-    except binascii.Error as e:
-        logger.info(f"{data[0:100]=} => {e}")
-        return None
-    except Exception as e:
-        logger.warning(f"{data[0:100]=} => {e}")
-        return None
-    try:
-        u, p = decoded.split(":", maxsplit=1)
-    except ValueError as e:
-        logger.info(f"{decoded[0:100]=} => {e}")
-        return None
-
-    return authenticate(request, username=u, password=p)
 
 
 def _enrich_user(user):
